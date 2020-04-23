d9ca0d35e4f7f7d2da6fcd7121fcc0fb31a84278
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-22 18:09:17 -0600

    Added link to semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import Button from "./components/Button";
+import NewsFeed from "./NewsFeed";
+import Profile from "./Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  };
+
+  render() {
+    return (
+      <div className="App"> 
+        <InputField />
+        <Button label={"Login"} />
+        <Button label={"Register"} />
+        {this.state.isLoggedIn ? <NewsFeed /> && <Profile /> : null }
+      </div>
+    );
+  }
 }
 
 export default App;
