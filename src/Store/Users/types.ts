7f46143b727fbca77652a166752fe850db9d7151
Karan Pandher <karanpandher@live.ca>
2020-04-24 12:14:32 -0600

    Types file done

--- src/Store/Users/types.ts
+++ src/Store/Users/types.ts
@@ -1,11 +1,9 @@
-export interface UsersState {
-
-}
-
+// Define the models of the State
 export interface Users {
     id: number; // general user id; unique identifcator  
-    username: string; 
+    userName: string; 
     password: string; 
+    // picture: Can add a profile picture here 
     firstName: string;
     lastName: string;
     email: string;
@@ -15,12 +13,17 @@ export interface Users {
 export interface Posts { 
     post: string; 
     quantity: number;
+    time: Date; 
 
+}
+export interface UsersState {
+    userNames: Users[] // An array for all the usernames stored 
+    NumberOfPost: Posts[] // An array for all the posts for each username 
 }
 
 // Define the type of actions 
-export const ADD_NEW_POST = 'ADD_NEW_POST'
-export const REMOVE_NEW_POST = 'REMOVE_NEW_POST'
+export const ADD_NEW_POST = 'ADD_NEW_POST';
+export const REMOVE_NEW_POST = 'REMOVE_NEW_POST';
 
 interface AddNewPost {
     type: typeof ADD_NEW_POST,
