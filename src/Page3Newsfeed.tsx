fe25a7ad6b5569a67f2b3d4f54769ebea76a224c
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-26 13:01:35 -0600

    Added text retrieval and random id generator

--- src/Page3Newsfeed.tsx
+++ src/Page3Newsfeed.tsx
@@ -16,27 +16,46 @@ export interface Page3Props {
 
 export class Page3 extends React.Component<Page3Props> {
 
-  newBark(e: any) 
-  {
+  // This generates the random id. 
+  randomID = (): number => {
+    let randomNumber: number = Math.floor(Math.random()*100);
+    randomNumber += this.props.posts.length;
+    return randomNumber;
+  };
+
+  // This will add a new "post" or in our case a new "bark".
+  newBark(e: any) {
     e.preventDefault();
-    addPost( { 
+
+    // This grabs the text value from our form.
+    const textField: HTMLInputElement | null = document.querySelector(
+      '[posted="postedtext"]'
+    );
+    let textFieldValue: string = " ";
+
+    if (textField !== null) textFieldValue = textField.value;
+
+    addPost({
       id: 2,
-      posted: "Some more tests"
-    } );
+      posted: textFieldValue,
+    });
   }
 
   public render() {
     return (
       <Fragment>
         <MessageExampleList />
-        <h1>Welcome to the Doge Park</h1><br />
+        <h1>Welcome to the Doge Park</h1>
+        <br />
         <ItemExampleExtraContent />
         <Form onSubmit={this.newBark}>
           <TextArea placeholder="Tell us more" />
         </Form>
         <br />
-        {this.props.posts.map(element => (<div>{element.posted}</div>))}
-        <Input type="submit" value="Bark" />      
+        {this.props.posts.map((element) => (
+          <div>{element.posted}</div>
+        ))}
+        <Input type="submit" value="Bark" />
       </Fragment>
     );
   }
