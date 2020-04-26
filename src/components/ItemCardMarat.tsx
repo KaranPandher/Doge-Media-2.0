0bac9c7d26063eaf5b2f3cd4e969c1217036d7d2
Karan Pandher <karanpandher@live.ca>
2020-04-26 02:00:11 -0600

    Adding followers to Item Card Marat

--- src/components/ItemCardMarat.tsx
+++ src/components/ItemCardMarat.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Item} from "semantic-ui-react";
+import {Item, Icon} from "semantic-ui-react";
 
 const description= [
     'Mauris at justo sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus rhoncus neque sapien, egestas varius diam vulputate nec. Nulla eget odio aliquam, mollis lectus sit amet, bibendum nisl. Nunc feugiat magna ac orci ullamcorper, vestibulum euismod mauris ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras convallis ipsum eget dui consectetur, eget eleifend dolor molestie. Nunc sit amet posuere ex. Fusce eget pharetra purus. Suspendisse potenti. In eleifend ex id nulla semper congue. Sed tincidunt ullamcorper neque id lacinia. Nulla efficitur imperdiet consectetur. Morbi rutrum vel orci nec malesuada.'
@@ -12,6 +12,8 @@ const ItemCardMarat = () => (
 
       <Item.Content>
         <Item.Header as='a'>Cute Dog</Item.Header>
+        <p></p>
+        <p>{extra}</p>
         <Item.Description>
           <p>{description}</p>
           <p>
@@ -22,6 +24,13 @@ const ItemCardMarat = () => (
       </Item.Content>
     </Item>
     </Item.Group>
+)
+
+const extra = (
+  <div>
+    <Icon name='user' />
+    3000 Followers
+  </div>
 )
 
 export default ItemCardMarat
