4a83bc0c2c8f76b82f0df7fbb1d9b51b6e54d2e6
Karan Pandher <karanpandher@live.ca>
2020-04-26 02:46:23 -0600

    Playing around with meta tags Stephane

--- src/components/ItemCardStephane.tsx
+++ src/components/ItemCardStephane.tsx
@@ -5,6 +5,8 @@ const description= [
     'Mauris at justo sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus rhoncus neque sapien, egestas varius diam vulputate nec. Nulla eget odio aliquam, mollis lectus sit amet, bibendum nisl. Nunc feugiat magna ac orci ullamcorper, vestibulum euismod mauris ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras convallis ipsum eget dui consectetur, eget eleifend dolor molestie. Nunc sit amet posuere ex. Fusce eget pharetra purus. Suspendisse potenti. In eleifend ex id nulla semper congue. Sed tincidunt ullamcorper neque id lacinia. Nulla efficitur imperdiet consectetur. Morbi rutrum vel orci nec malesuada.'
 ].join(' ')
 
+const meta = [ 'You wanna start a podcast?'].join(' ')
+
 const ItemCardStephane = () => (
     <Item.Group>
     <Item>
@@ -13,6 +15,7 @@ const ItemCardStephane = () => (
         <Item.Header as='a'>Cute Dog</Item.Header>
         <p></p>
         <p>{extra}</p>
+        <p> {meta} </p>
         <Item.Description>
           <p>{description}</p>
           <p>
