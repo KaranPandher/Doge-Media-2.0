e6a624078aef16d93dac5ce2bdd80597b120cd4c
Karan Pandher <karanpandher@live.ca>
2020-04-26 02:03:06 -0600

    Added followers to rest of item cards

--- src/components/ItemCardStephane.tsx
+++ src/components/ItemCardStephane.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Item} from "semantic-ui-react";
+import {Item, Icon} from "semantic-ui-react";
 
 const description= [
     'Mauris at justo sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus rhoncus neque sapien, egestas varius diam vulputate nec. Nulla eget odio aliquam, mollis lectus sit amet, bibendum nisl. Nunc feugiat magna ac orci ullamcorper, vestibulum euismod mauris ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras convallis ipsum eget dui consectetur, eget eleifend dolor molestie. Nunc sit amet posuere ex. Fusce eget pharetra purus. Suspendisse potenti. In eleifend ex id nulla semper congue. Sed tincidunt ullamcorper neque id lacinia. Nulla efficitur imperdiet consectetur. Morbi rutrum vel orci nec malesuada.'
@@ -9,9 +9,10 @@ const ItemCardStephane = () => (
     <Item.Group>
     <Item>
       <Item.Image size= 'large' src='../img/ProfilePictures/dogelogo.png' />
-
       <Item.Content>
         <Item.Header as='a'>Cute Dog</Item.Header>
+        <p></p>
+        <p>{extra}</p>
         <Item.Description>
           <p>{description}</p>
           <p>
@@ -22,6 +23,13 @@ const ItemCardStephane = () => (
       </Item.Content>
     </Item>
     </Item.Group>
+)
+
+const extra = (
+  <div>
+    <Icon name='user' />
+    3000 Followers
+  </div>
 )
 
 export default ItemCardStephane
