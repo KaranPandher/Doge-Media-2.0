84b2c0307d14342559b4776f577b695e32f6aa2b
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-27 10:32:56 -0600

    Login now routes to newfeed. Cleaned up unused imports

--- src/components/InputLogin.tsx
+++ src/components/InputLogin.tsx
@@ -1,45 +1,45 @@
 import React from "react";
-import { Form, Grid, Header, Image , Button} from "semantic-ui-react";
-import loginIcon from '../img/login-icon.png';
+import { Form, Grid, Header, Image, Button } from "semantic-ui-react";
+import loginIcon from "../img/login-icon.png";
 
 const FormExampleField = () => (
-  <Grid textAlign = 'center' verticalAlign = 'middle'>
+  <Grid textAlign="center" verticalAlign="middle">
     <Grid.Column style={{ maxWidth: 600 }}>
-      <Header as = 'h2' color = 'teal' textAlign = 'center'>
-        <Image src = {loginIcon}/> Login to your account 
+      <Header as="h2" color="teal" textAlign="center">
+        <Image src={loginIcon} /> Login to your account
       </Header>
-  <Form>
-    <Form.Input
-      fluid
-      icon = 'user'
-      iconPosition = 'left'
-      label="Email"
-      placeholder="Email"
-      id="form-input-email"
-    />
-    <Form.Input
-      fluid
-      icon = 'lock'
-      iconPosition = 'left'
-      label="Password"
-      placeholder="Password"
-      type = "password"
-    />
-    <br></br>
-    <Form.Checkbox
-      label="Are you a human?"
-      error={{
-        content: "We want to avoid bots populating our site",
-        pointing: "left",
-      }}
-    />
-    <br></br>
-  </Form>
+      <Form>
+        <Form.Input
+          fluid
+          icon="user"
+          iconPosition="left"
+          label="Email"
+          placeholder="Email"
+          id="form-input-email"
+        />
+        <Form.Input
+          fluid
+          icon="lock"
+          iconPosition="left"
+          label="Password"
+          placeholder="Password"
+          type="password"
+        />
+        <br></br>
+        <Form.Checkbox
+          label="Are you a human?"
+          error={{
+            content: "We want to avoid bots populating our site",
+            pointing: "left",
+          }}
+        />
+        <br></br>
+      </Form>
 
-      <Button color = 'teal' fluid size = 'large'>
+      <Button color="teal" fluid size="large" href="../Page3Newsfeed">
         Login
       </Button>
-  </Grid.Column>
+    </Grid.Column>
   </Grid>
 );
 
--- src/components/ProfileCardDoge.tsx
+++ src/components/ProfileCardDoge.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Icon, Item} from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react'
 // import stephane from '../img/ProfilePictures/stephane.jpg'
 import doge from '../img/ProfilePictures/dogeprofilepicture.png'
 
