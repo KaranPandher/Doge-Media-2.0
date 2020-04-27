919b416d2b5cd3b5c0976ddc789ad1ab7dce6978
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-04-27 13:35:50 -0600

    fixed newsfeed barks

--- src/components/NewsFeedContent.tsx
+++ src/components/NewsFeedContent.tsx
@@ -18,7 +18,7 @@ export default class NewsFeedContent extends React.Component<NewsFeedProps> {
     return (
       <Grid>
         <Item.Group>
-          <br/>
+          <br />
           <Item>
             <Item.Image
               as="a"
@@ -26,23 +26,34 @@ export default class NewsFeedContent extends React.Component<NewsFeedProps> {
               size="small"
               src={stephane}
             />
-
             <Item.Content>
-              <Item.Header as="a" href="../Profile-Stephane">
+              <Item.Header
+                as="a"
+                href="../Profile-Stephane"
+                style={{ maxWidth: 550 }}
+              >
                 Mishu
               </Item.Header>
-              <Item.Meta>
-                Human: Stephane / Breed: German Shepherd Malamute / 2X <Icon name="trophy" color="yellow" /> YEG Fetch Champ
+              <Item.Meta style={{ maxWidth: 550 }}>
+                Human: Stephane / Breed: German Shepherd Malamute / 2X{" "}
+                <Icon name="trophy" color="yellow" /> YEG Fetch Champ
               </Item.Meta>
               <Item.Content style={{ maxWidth: 550 }}>
-                I can't believe i lost my owners ball at the park again! It's an orange ball that has the word "Step" marking on it! No idea what it means. My owner said he won't get me another one till this is found! Please Re-Bark!
+                I can't believe i lost my owners ball at the park again! It's an
+                orange ball that has the word "Step" marking on it! No idea what
+                it means. My owner said he won't get me another one till this is
+                found! Please Re-Bark!
                 <br />
                 <br />
-                #WhosAGoodDog #BarksUp #FetchLife #AtTheParkWithMyWoes #DownWithCollars
+                #WhosAGoodDog #BarksUp #FetchLife #AtTheParkWithMyWoes
+                #DownWithCollars
               </Item.Content>
               <Item.Extra style={{ maxWidth: 550 }}>
-              <Icon color="blue" name='food'/>Treats <Icon color="blue" name="headphones" />Re-Bark! <br/>
-               <Icon color="green" name="plus"/> <Icon color="red" name="minus" /> 50 Votes 
+                <Icon color="blue" name="food" />
+                Treats <Icon color="blue" name="headphones" />
+                Re-Bark! <br />
+                <Icon color="green" name="plus" />{" "}
+                <Icon color="red" name="minus" /> 50 Votes
               </Item.Extra>
             </Item.Content>
           </Item>
@@ -54,14 +65,31 @@ export default class NewsFeedContent extends React.Component<NewsFeedProps> {
               size="small"
               src={karan}
             />
-
             <Item.Content>
-              <Item.Header as="a" href="../Profile-Karan">
-                Karan Redux
+              <Item.Header
+                as="a"
+                href="../Profile-Karan"
+                style={{ maxWidth: 550 }}
+              >
+                Rosie
               </Item.Header>
-              <Item.Description>{paragraph}</Item.Description>
-              <Item.Extra>
-                <Icon color="green" name="plus" />
+              <Item.Meta style={{ maxWidth: 550 }}>
+                Human: Karan / Breed: Pitbull / Destroyer of Chew Toys{" "}
+                <Icon name="football ball" color="blue" />{" "}
+                <Icon name="hand lizard outline" color="blue" />
+              </Item.Meta>
+              <Item.Content style={{ maxWidth: 550 }}>
+                Got a toy that needs chewing? Need a running partner? Look no
+                futher! i'm your dog!
+                <br />
+                <br />
+                #ChewToysDontStandAChance #BarksUp #FetchLife
+              </Item.Content>
+              <Item.Extra style={{ maxWidth: 550 }}>
+                <Icon color="blue" name="food" />
+                Treats <Icon color="blue" name="headphones" />
+                Re-Bark! <br />
+                <Icon color="green" name="plus" />{" "}
                 <Icon color="red" name="minus" /> 150 Votes
               </Item.Extra>
             </Item.Content>
